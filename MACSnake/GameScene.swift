23b8b127696b00a4a20abd77833cf203d07a9d0c
ikochylo1431 <ikochylo1431@stu.d214.org>
2018-05-18 11:17:06 -0500

    Snake MVP

--- MACSnake/GameScene.swift
+++ MACSnake/GameScene.swift
@@ -7,86 +7,143 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
+    fileprivate var snakeCurrentDirection: direction = .right
+    fileprivate var snake: [SKSpriteNode] = [.snakeBodyPart]
+    fileprivate var snakePosition: [CGPoint] = [.zero]
+    fileprivate var oldUpdateTime: TimeInterval = 0
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
     
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+        prepareScene()
+    }
+    
+    fileprivate func updatePositionOfSnake() {
+        for i in 0..<snakePosition.count {
+            if i < snake.count {
+                let bodyPart = snake[i]
+                bodyPart.position = snakePosition[i]
+            } else { break }
+        }
+        
+    }
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        //
+    }
+    
+}
+
+
+enum direction: UInt16 {
+    case right = 0x7C
+    case left = 0x7B
+    case up = 0x7E
+    case down = 0x7D
+}
+
+extension SKSpriteNode {
+    static var snakeBodyPart: SKSpriteNode {
+        let bodyPart = SKSpriteNode(color: .yellow, size: CGSize(width: 50, height: 50))
+        return bodyPart
+    }
+    
+}
+
+extension GameScene {
+    func prepareScene() {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        for bodyPart in snake {
+            addChild(bodyPart)
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
-    }
-    
-    override func keyDown(with event: NSEvent) {
-        switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
-        default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
-        }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
+
+
+extension GameScene {
+    fileprivate func movement() {
+        switch snakeCurrentDirection {
+        case .right:
+            guard let headPosition = snakePosition.first else { return }
+            snakePosition.insert(headPosition.pointToRight, at: 0)
+        case .left:
+            guard let headPosition = snakePosition.first else { return }
+            snakePosition.insert(headPosition.pointToLeft, at: 0)
+        case .up:
+            guard let headPosition = snakePosition.first else { return }
+            snakePosition.insert(headPosition.pointToUp, at: 0)
+        case .down:
+            guard let headPosition = snakePosition.first else { return }
+            snakePosition.insert(headPosition.pointToDown, at: 0)
+        }
+    }
+}
+
+extension GameScene {
+    override func update(_ currentTime: TimeInterval) {
+        let delta: TimeInterval = 0.5
+        if currentTime - oldUpdateTime > delta {
+            oldUpdateTime = currentTime
+            movement()
+            updatePositionOfSnake()
+        }
+    }
+}
+extension CGPoint {
+    static var normalizedMiddle: CGPoint { return CGPoint(x: 0.5, y: 0.5)}
+    
+    func offSetBy(_ point: CGPoint) -> CGPoint {
+        return CGPoint(x: x + point.x, y: y + point.y)
+    }
+    
+    var pointToLeft: CGPoint{
+        return self.offSetBy(CGPoint(x: -CGSize(width: 50, height: 50).width, y: 0))
+    }
+    
+    var pointToRight: CGPoint{
+        return self.offSetBy(CGPoint(x: CGSize(width: 50, height: 50).width, y: 0))
+    }
+    
+    var pointToUp: CGPoint{
+        return self.offSetBy(CGPoint(x: 0, y: CGSize(width: 50, height: 50).height))
+    }
+    
+    var pointToDown: CGPoint{
+        return self.offSetBy(CGPoint(x: 0, y: -CGSize(width: 50, height: 50).height))
+    }
+    
+}
+
+extension GameScene {
+    override func keyDown(with event: NSEvent) {
+        switch event.keyCode {
+        case direction.down.rawValue:
+            snakeCurrentDirection = .down
+        case direction.up.rawValue:
+            snakeCurrentDirection = .up
+        case direction.right.rawValue:
+            snakeCurrentDirection = .right
+        case direction.left.rawValue:
+            snakeCurrentDirection = .left
+
+        default: break
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
